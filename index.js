b181a3d673b4399e7434a16a9f5d928a5fe05aee
321-bip <79604277+321-bip@users.noreply.github.com>
2021-10-07 21:08:36 -0300

    correçao da function testeVitoria()

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let el;
 let id;
 let cedulas;
 let rodada = true;
+let jogadas = 0;
+let velha = true
 const tabuleiro = [ ["","",""],
                     ["","",""],
                     ["","",""], ]  
@@ -32,7 +34,7 @@ document.querySelectorAll('button.cedula').forEach(function(button){
           cedulas = document.getElementById(id).style.backgroundImage = img;
                     document.getElementById(id).disabled = true
       let jogador = rodada == true ? jogadorX : jogadorO    
-                
+              
         switch (id)
          {
            case "0":
@@ -64,82 +66,99 @@ document.querySelectorAll('button.cedula').forEach(function(button){
            break;
          }
              rodada = !rodada
-             console.log(id)
-             console.table(tabuleiro) 
-             testeVitoria()
-}
+             jogadas+=1
+             console.log(jogadas)
+             console.table(tabuleiro)
+            
+                testeVitoria()
+            
+  }
 
 function testeVitoria()
  {
-    if(tabuleiro[0][0] == jogadorX && tabuleiro[0][1] == jogadorX && tabuleiro[0][2])
+    if(tabuleiro[0][0] == jogadorX && tabuleiro[0][1] == jogadorX && tabuleiro[0][2] == jogadorX)
      {
-                 alert(jogadorX)
+       velha = false
+       alert(jogadorX)
      }
-    else if(tabuleiro[1][0] == jogadorX && tabuleiro[1][1] == jogadorX && tabuleiro[1][2])
+    else if(tabuleiro[1][0] == jogadorX && tabuleiro[1][1] == jogadorX && tabuleiro[1][2] == jogadorX)
      {
-                alert(jogadorX)
+       velha = false
+       alert(jogadorX)
      }
-    else if(tabuleiro[2][0] == jogadorX && tabuleiro[2][1] == jogadorX && tabuleiro[2][2])
+    else if(tabuleiro[2][0] == jogadorX && tabuleiro[2][1] == jogadorX && tabuleiro[2][2] == jogadorX)
      {
-                alert(jogadorX)
+       velha = false
+       alert(jogadorX)
      }
-    else if(tabuleiro[0][0] == jogadorX && tabuleiro[1][0] == jogadorX && tabuleiro[2][0])
+    else if(tabuleiro[0][0] == jogadorX && tabuleiro[1][0] == jogadorX && tabuleiro[2][0] == jogadorX)
      {
-                 alert(jogadorX)
+        velha = false
+        alert(jogadorX)
      }
-    else if(tabuleiro[0][1] == jogadorX && tabuleiro[1][1] == jogadorX && tabuleiro[2][1])
+    else if(tabuleiro[0][1] == jogadorX && tabuleiro[1][1] == jogadorX && tabuleiro[2][1] == jogadorX)
      {
-                alert(jogadorX)
+       velha = false
+       alert(jogadorX)
      }
-    else if(tabuleiro[0][2] == jogadorX && tabuleiro[1][2] == jogadorX && tabuleiro[2][2])
+    else if(tabuleiro[0][2] == jogadorX && tabuleiro[1][2] == jogadorX && tabuleiro[2][2] == jogadorX)
      {
-                alert("6")
+       velha = false
+       alert(jogadorX)
      }
-    else if(tabuleiro[0][2] == jogadorX && tabuleiro[1][1] == jogadorX && tabuleiro[2][0])
+    else if(tabuleiro[0][2] == jogadorX && tabuleiro[1][1] == jogadorX && tabuleiro[2][0] == jogadorX)
      {
-                alert(jogadorX)
+       velha = false
+       alert(jogadorX)
      }
-    else if(tabuleiro[0][0] == jogadorX && tabuleiro[1][1] == jogadorX && tabuleiro[2][2])
+    else if(tabuleiro[0][0] == jogadorX && tabuleiro[1][1] == jogadorX && tabuleiro[2][2] == jogadorX)
      {
-                alert(jogadorX)
+       velha = false
+       alert(jogadorX)
      }
-     if(tabuleiro[0][0] == jogadorO && tabuleiro[0][1] == jogadorO && tabuleiro[0][2])
+     if(tabuleiro[0][0] == jogadorO && tabuleiro[0][1] == jogadorO && tabuleiro[0][2] == jogadorO)
      {
-                 alert(jogadorO)
+        velha = false
+        alert(jogadorO)
      }
-    else if(tabuleiro[1][0] == jogadorO && tabuleiro[1][1] == jogadorO && tabuleiro[1][2])
+    else if(tabuleiro[1][0] == jogadorO && tabuleiro[1][1] == jogadorO && tabuleiro[1][2] == jogadorO)
      {
                 alert(jogadorO)
      }
-    else if(tabuleiro[2][0] == jogadorO && tabuleiro[2][1] == jogadorO && tabuleiro[2][2])
+    else if(tabuleiro[2][0] == jogadorO && tabuleiro[2][1] == jogadorO && tabuleiro[2][2] == jogadorO)
      {
-                alert(jogadorO)
+       velha = false
+       alert(jogadorO)
      }
-    else if(tabuleiro[0][0] == jogadorO && tabuleiro[1][0] == jogadorO && tabuleiro[2][0])
+    else if(tabuleiro[0][0] == jogadorO && tabuleiro[1][0] == jogadorO && tabuleiro[2][0] == jogadorO)
      {
-                 alert(jogadorO)
+        velha = false
+        alert(jogadorO)
      }
-    else if(tabuleiro[0][1] == jogadorO && tabuleiro[1][1] == jogadorO && tabuleiro[2][1])
+    else if(tabuleiro[0][1] == jogadorO && tabuleiro[1][1] == jogadorO && tabuleiro[2][1] == jogadorO)
      {
-                alert(jogadorO)
+       velha = false
+       alert(jogadorO)
      }
-    else if(tabuleiro[0][2] == jogadorO && tabuleiro[1][2] == jogadorO && tabuleiro[2][2])
+    else if(tabuleiro[0][2] == jogadorO && tabuleiro[1][2] == jogadorO && tabuleiro[2][2] == jogadorO)
      {
-                alert(jogadorO)
+       velha = false
+       alert(jogadorO)
      }
-    else if(tabuleiro[0][2] == jogadorO && tabuleiro[1][1] == jogadorO && tabuleiro[2][0])
+    else if(tabuleiro[0][2] == jogadorO && tabuleiro[1][1] == jogadorO && tabuleiro[2][0] == jogadorO)
      {
-                alert(jogadorO)
+        velha = false
+        alert(jogadorO)
      }
-    else if(tabuleiro[0][0] == jogadorO && tabuleiro[1][1] == jogadorO && tabuleiro[2][2])
+    else if(tabuleiro[0][0] == jogadorO && tabuleiro[1][1] == jogadorO && tabuleiro[2][2] == jogadorO)
      {
-                alert(jogadorO)
+        velha = false
+        alert(jogadorO)
      }
-    else
+    else if(velha == true && jogadas == 9)
      {
-       alert("jogo enpatado")
-     }               
-
+       alert("enpato")
+     } 
  }
 
              
