3958dd2a8ec5fae35641a4308f6aa16aaa27dc81
321-bip <79604277+321-bip@users.noreply.github.com>
2021-10-07 22:14:14 -0300

    jj

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ let velha = true
 const tabuleiro = [ ["","",""],
                     ["","",""],
                     ["","",""], ]  
-const conbinasoesVitoria = [ [0,1,2],
+/*const conbinasoesVitoria = [ [0,1,2],
                              [3,4,5],
                              [6,7,8],
                              [0,3,6],
                              [1,4,7],
                              [2,5,8],
                              [0,4,8],
-                             [2,4,6], ];
+                             [2,4,6], ];*/
                            
 document.querySelectorAll('button.cedula').forEach(function(button){
     
